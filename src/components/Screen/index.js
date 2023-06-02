fd783b174f0b3f3983274c6c9d10283ca8df990f
Deepak Singh <singhdeepak5219@gmail.com>
2023-06-03 01:10:44 +0530

    modified screen

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,11 +1,11 @@
-import { Textfit } from "react-textfit";
+
 import "./style.css"
 
 const Screen = ({value}) => {
   return (
-    <Textfit className="screen" mode="single" max={70}>
+    <div className="screen" >
       {value}
-    </Textfit>
+    </div>
   );
 };
 
